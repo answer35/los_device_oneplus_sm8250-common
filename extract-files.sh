92656e9e95ebbc90877af16c6daef711be94c440
Yuan Si <do4suki@gmail.com>
2022-07-30 15:42:04 +0800

    sm8250-common: Switch to QTI components
    Signed-off-by: Yuan Si <do4suki@gmail.com>
    Change-Id: I87e80151cf945f4e804253106215a0ed770b7945

--- extract-files.sh
+++ extract-files.sh
@@ -75,10 +75,11 @@ function blob_fixup() {
         product/etc/sysconfig/com.android.hotwordenrollment.common.util.xml)
             sed -i "s/\/my_product/\/product/" "${2}"
             ;;
-        system_ext/lib64/libwfdnative.so)
-            sed -i "s/android.hidl.base@1.0.so/libhidlbase.so\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00/" "${2}"
+        vendor/etc/libnfc-mtp-SN100.conf)
+            sed -i "s/^NXP_RF_CONF_BLK_9/#NXP_RF_CONF_BLK_9/" "${2}"
+            sed -i "s/^NXP_RF_CONF_BLK_10/#NXP_RF_CONF_BLK_10/" "${2}"
             ;;
-        vendor/etc/msm_irqbalance.conf)
+	vendor/etc/msm_irqbalance.conf)
             sed -i "s/IGNORED_IRQ=27,23,38$/&,115,332/" "${2}"
             ;;
         vendor/lib64/hw/com.qti.chi.override.so)
